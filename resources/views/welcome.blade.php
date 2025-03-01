009ec97f547072d5ee2c733df5a978e85d1bd181
marty <sokoleskam@outlook.com>
2025-03-01 17:22:55 +0100

    directories added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hello</h1>
+</body>
+</html>
